148145d60d1429e15aa95ebc3d9c50b20f50d02d
Lucas Benine <lucasbenine41@gmail.com>
2022-06-14 20:23:11 -0300

    Aligning product form fields

--- frontend-vendas/src/components/produtos/cadastro/index.tsx
+++ frontend-vendas/src/components/produtos/cadastro/index.tsx
@@ -3,64 +3,73 @@ import { Layout } from "components";
 export const CadastroProdutos: React.FC = () => {
   return (
     <Layout titulo="Cadastro de Produtos">
-      <div className="field">
-        <label className="label" htmlFor="inputSKU">
-          SKU: *
-        </label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder="Digite o SKU do produto"
-            id="inputSKU"
-          />
+      <div className="columns">
+        <div className="field column is-half">
+            <label className="label" htmlFor="inputSKU">
+            SKU: *
+            </label>
+            <div className="control">
+            <input
+                className="input"
+                type="text"
+                placeholder="Digite o SKU do produto"
+                id="inputSKU"
+            />
+            </div>
+        </div>
+        <div className="field column is-half">
+            <label className="label" htmlFor="inputPreco">
+            Preço: *
+            </label>
+            <div className="control">
+            <input
+                className="input"
+                type="text"
+                placeholder="Digite o Preço do produto"
+                id="inputPreco"
+            />
+            </div>
+        </div>
+      </div>  
+
+      <div className="columns">
+        <div className="field column is-full">
+            <label className="label" htmlFor="inputNome">
+            Nome: *
+            </label>
+            <div className="control">
+            <input
+                className="input"
+                type="text"
+                placeholder="Digite o Nome do produto"
+                id="inputNome"
+            />
+            </div>
         </div>
       </div>
-      <div className="field">
-        <label className="label" htmlFor="inputNome">
-          Nome: *
-        </label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder="Digite o Nome do produto"
-            id="inputNome"
-          />
+
+      <div className="columns">
+        <div className="field column is-full">
+            <label className="label" htmlFor="InputDescricao">
+            Descrição: *
+            </label>
+            <div className="control">
+            <textarea
+                className="textarea"
+                placeholder="Escreva a descrição detalhada do produto"
+                id="InputDescricao"
+            ></textarea>
+            </div>
         </div>
       </div>
-      <div className="field">
-        <label className="label" htmlFor="inputPreco">
-          Preço: *
-        </label>
-        <div className="control">
-          <input
-            className="input"
-            type="text"
-            placeholder="Digite o Preço do produto"
-            id="inputPreco"
-          />
-        </div>
-      </div>
-      <div className="field">
-        <label className="label" htmlFor="InputDescricao">
-          Descrição: *
-        </label>
-        <div className="control">
-          <textarea
-            className="textarea"
-            placeholder="Digite o Preço do produto"
-            id="InputDescricao"
-          ></textarea>
-        </div>
-      </div>
-      <div className="field is-grouped">
-        <div className="control">
-          <button className="button is-link">Salvar</button>
-        </div>
-        <div className="control">
-          <button className="button is-link is-light">Cancelar</button>
-        </div>
+
+        <div className="field is-grouped">
+            <div className="control">
+                <button className="button is-link">Salvar</button>
+            </div>
+            <div className="control">
+                <button className="button is-link is-light">Cancelar</button>
+            </div>
       </div>
     </Layout>
   );
